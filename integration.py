864a549070b9f09db76c717ab5e997b7b9fcea1c
bmouw <941859+bmouw@users.noreply.github.com>
2023-07-24 12:33:02 -0500

    Initial load for this integration

---
+++ integration.py
@@ -0,0 +1,15 @@
+import cloudbeds_api
+import seam_api
+
+#def handle_cb_callback():
+#handle the callback request when a new property is added
+
+
+#def reservation_checkin(): 
+    #action to take when a reservation is checked in
+
+#def write_mapping(property_id, mapping):
+#write the mapping for 
+
+#def load_mapping(property_id):
+# load the mapping for 
---
+++ seam.py
@@ -0,0 +1,5 @@
+def request_door_code(door_id):
+    print('something')
+
+def fetch_doors():
+   print('something')
